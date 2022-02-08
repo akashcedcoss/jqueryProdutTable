f8387aec3c4a543d8df157b68598d5835986c9aa
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-08 18:45:05 +0530

    first commit

---
+++ src/style.js
@@ -0,0 +1,45 @@
+var product=[];
+function addProduct(){
+    var id = document.getElementById('pID').value;
+    var name1 = document.getElementById('pName').value;
+    var price = document.getElementById('pPrice').value;
+    var quantity = document.getElementById('pQuantity').value;
+    checkValue(id, name1, price, quantity);
+    display(product);
+}
+function checkValue(id , name1 , price, quantity){
+    for(var i = 0;i<product.length;i++){
+        console.log(product[i]);
+        if (product[i].pID == id)
+            {
+                     alert("Product ID already exists or Invalid Input");
+                 return false;
+            }
+    }
+    obj= {};
+    obj['pID'] = id;
+    obj['pName'] = name1;
+    obj['pPrice'] = price;
+    obj['pQuantity'] = quantity;
+    product.push(obj);
+}
+function display(result){
+var text = document.getElementById('table1');
+text.innerHTML = "<tr>\
+     <td>SKU</td>\
+     <td>Product Name</td>\
+     <td>Product Price</td>\
+     <td>Product Quantity</td>\
+     <td>Action</td>\
+    </tr>";
+    for(let i =0; i<result.length;i++){
+        text.innerHTML+='<tr>\
+         <td>' + result[i].pID + '</td>\
+         <td>' + result[i].pName + '</td>\
+         <td>' + result[i].pPrice + '</td>\
+         <td>' + result[i].pQuantity + '</td>\
+         <td> <a href="#" onclick=editForm()>Edit</a><a href="#" onclick=deleteForm()>Delete</a></td>\
+     </tr>';
+    }
+    
+}
